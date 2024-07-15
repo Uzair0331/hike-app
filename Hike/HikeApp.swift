2fedb39ff481a38c01c3cc9bcd1fa07340eda684
uzair0331 <uxair0331xhaikhgmail.com>
2024-07-15 15:32:57 +0500

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by App Tycoons on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
